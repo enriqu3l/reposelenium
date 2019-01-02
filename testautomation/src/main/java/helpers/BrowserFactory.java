ff8fe662de7ff7c18c5f47184d363c60e0755123
enrique.lopez <enrique.lopez@pricetravel.com.mx>
2019-01-02 10:41:35 -0600

    Added "if" for cancel notifications in firefox and chrome

--- testautomation/src/main/java/helpers/BrowserFactory.java
+++ testautomation/src/main/java/helpers/BrowserFactory.java
@@ -9,6 +9,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.testng.Assert;
+
+import config.FrameworkConfig;
 
 //Para poder usar BrowserFactory es necesario setear el path de los drivers
 //en las variables del sistema
@@ -17,28 +21,43 @@ public class BrowserFactory {
 	public static WebDriver driver;
 	
 	public static WebDriver StartBrowser(String BrowserName, String url) {
-		if(BrowserName.equals("firefox")) {
-			//Desabilitando las notificaciones
-			FirefoxProfile profile =new FirefoxProfile();
-			profile.setPreference("dom.webnotifications.enabled", false);
-			FirefoxOptions option=new FirefoxOptions();
-			option.setProfile(profile);
+		if(BrowserName.equalsIgnoreCase(FrameworkConfig.BROWSER_FIREFOX)) {
+			if(FrameworkConfig.BROWSER_CANCELNOTIFICATIONS) {
+				//Desabilitando las notificaciones
+				FirefoxProfile profile =new FirefoxProfile();
+				profile.setPreference("dom.webnotifications.enabled", false);
+				FirefoxOptions option=new FirefoxOptions();
+				option.setProfile(profile);
+				driver = new FirefoxDriver(option);
+			}else {
+				driver = new FirefoxDriver();
+			}
 			
-			driver = new FirefoxDriver(option);
 		}
-		else if(BrowserName.equals("chrome")) {
+		else if(BrowserName.equalsIgnoreCase(FrameworkConfig.BROWSER_CHROME)) {
+			if(FrameworkConfig.BROWSER_CANCELNOTIFICATIONS) {
+				//Opening chrome with notification disabled
+				ChromeOptions ops = new ChromeOptions();
+	            ops.addArguments("--disable-notifications");
+				driver = new ChromeDriver(ops);
+			}else {
+				driver = new ChromeDriver();
+			}
 			
-			//Opening chrome with notification disabled
-			ChromeOptions ops = new ChromeOptions();
-            ops.addArguments("--disable-notifications");
-			driver = new ChromeDriver(ops);
 		}
-		else if(BrowserName.equalsIgnoreCase("IE")) {
+		else if(BrowserName.equalsIgnoreCase(FrameworkConfig.BROWSER_IE)) {
+			//Me falta agregar el if de la cancelacion de notificaciones en ie
 			driver = new InternetExplorerDriver();
+		}
+		else if(BrowserName.equalsIgnoreCase(FrameworkConfig.BROWSER_OPERA)) {
+			//Me falta agregar el if de la cancelacion de notificaciones en ie
+			driver = new OperaDriver();
+		}else{
+			Assert.fail("No se ha especificado el browser correctamente (solo puede ser: chrome, firefox, ie, opera)");
 		}
 		driver.manage().window().maximize();
 		driver.get(url);
-		driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		
 		return driver;
 	}
