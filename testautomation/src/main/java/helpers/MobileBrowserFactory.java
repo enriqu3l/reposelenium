42952e2694f6a2cdcb08df545a728ab8ac45feed
Enrique Lopez Magaña <enrique.lopez@pricetravel.com>
2019-02-12 22:48:19 -0600

    Disabled chrome notifications in MobileBrowserFactory

--- testautomation/src/main/java/helpers/MobileBrowserFactory.java
+++ testautomation/src/main/java/helpers/MobileBrowserFactory.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
@@ -13,7 +14,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-//Para poder usar BrowserFactory es necesario setear el path de los drivers
+//Para poder usar BrowserFactory es necesario setear el path de los webdrivers
 //en las variables del sistema
 
 public class MobileBrowserFactory {
@@ -27,10 +28,14 @@ public class MobileBrowserFactory {
 		if(_browserName.equalsIgnoreCase(FrameworkConfig.BROWSER_CHROME)) {
 			DesiredCapabilities capabilities = DesiredCapabilities.android();
 			capabilities.setCapability("autoDismissAlerts", true);
-			capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,BrowserType.CHROME);
 			capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,_device);
 			capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
 			capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, _version);
+			capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,BrowserType.CHROME);
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--disable-notifications");
+			capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+			
 			URL url = null;
 			try {
 				url = new URL("http://127.0.0.1:4723/wd/hub");
